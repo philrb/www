beffb6c1a05750df50e0419b41bd7e381473803a
philrb <157054211+philrb@users.noreply.github.com>
2024-02-04 20:03:06 -0800

    feat: set up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "_layouts/**/*.html",
+    "_posts/**/*.md",
+    "*.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
